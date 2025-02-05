723c69066db038259b501f2008a2c811cc080efc
schkuromi <vgcrox@gmail.com>
2025-02-05 21:00:50 +0800

    rearranged to be correct to loading order

--- src/mod.ts
+++ src/mod.ts
@@ -2,18 +2,46 @@ import { DependencyContainer } from "tsyringe";
 
 import { ILogger } from "@spt/models/spt/utils/ILogger";
 import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
+import { DatabaseServer } from "@spt/servers/DatabaseServer";
+import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
 import { IPostSptLoadMod } from "@spt/models/external/IPostSptLoadMod";
 import { ConfigServer } from "@spt/servers/ConfigServer";
 import { ConfigTypes } from "@spt/models/enums/ConfigTypes";
-import { DatabaseServer } from "@spt/servers/DatabaseServer";
-import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
+import { IRagfairConfig } from "@spt/models/spt/config/IRagfairConfig";
 import { LogTextColor } from "@spt/models/spt/logging/LogTextColor";
 import { LogBackgroundColor } from "@spt/models/spt/logging/LogBackgroundColor";
-import { IRagfairConfig } from "@spt/models/spt/config/IRagfairConfig";
 
 class Mod implements IPostSptLoadMod, IPostDBLoadMod
 {    
     private modConfig = require("../config/config.json")
+
+    public postDBLoad(container: DependencyContainer): void
+    {
+        // get database from the server
+        const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+
+        // get all the in-memory json foudn in /assets/database
+        const tables: IDatabaseTables = databaseServer.getTables();
+
+        // change database flea options
+
+        // option for enable the flea market
+        tables.globals.config.RagFair.enabled = this.modConfig.configOptions.fleaEnabled;
+
+        // option for setting min level for flea access
+        tables.globals.config.RagFair.minUserLevel = this.modConfig.configOptions.fleaMinimumLevel;
+
+        // option for forcing only FIR items to be sold on flea, rather than all items
+        tables.globals.config.RagFair.isOnlyFoundInRaidAllowed = this.modConfig.configOptions.fleaSellingFIROnly;
+
+        // log it if debug is enabled
+        if (this.modConfig.modDebug == true)
+        {
+            console.log("[DEBUG] [SCHKRM] Basic Flea Changer - Flea market button works:", this.modConfig.configOptions.fleaEnabled)
+            console.log("[DEBUG] [SCHKRM] Basic Flea Changer - Flea market minimum user level set to:", this.modConfig.configOptions.fleaMinimumLevel)
+            console.log("[DEBUG] [SCHKRM] Basic Flea Changer - Flea market only accepts FIR items set to:", this.modConfig.configOptions.fleaSellingFIROnly)
+        }        
+    }
 
     public postSptLoad(container: DependencyContainer): void
     {
@@ -71,37 +99,7 @@ class Mod implements IPostSptLoadMod, IPostDBLoadMod
             console.log("[DEBUG] [SCHKRM] Basic Flea Changer - Flea market item packs offers minimum offers per item:", fleaConfig.dynamic.pack.itemCountMin)
             console.log("[DEBUG] [SCHKRM] Basic Flea Changer - Flea market item packs offers maximum offers per item:", fleaConfig.dynamic.pack.itemCountMax)
         }
-    }
 
-    public postDBLoad(container: DependencyContainer): void
-    {
-        const logger = container.resolve<ILogger>("WinstonLogger");
-
-        // get database from the server
-        const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
-
-        // get all the in-memory json foudn in /assets/database
-        const tables: IDatabaseTables = databaseServer.getTables();
-
-        // change database flea options
-
-        // option for enable the flea market
-        tables.globals.config.RagFair.enabled = this.modConfig.configOptions.fleaEnabled;
-
-        // option for setting min level for flea access
-        tables.globals.config.RagFair.minUserLevel = this.modConfig.configOptions.fleaMinimumLevel;
-
-        // option for forcing only FIR items to be sold on flea, rather than all items
-        tables.globals.config.RagFair.isOnlyFoundInRaidAllowed = this.modConfig.configOptions.fleaSellingFIROnly;
-
-        // log it if debug is enabled
-        if (this.modConfig.modDebug == true)
-        {
-            console.log("[DEBUG] [SCHKRM] Basic Flea Changer - Flea market button works:", this.modConfig.configOptions.fleaEnabled)
-            console.log("[DEBUG] [SCHKRM] Basic Flea Changer - Flea market minimum user level set to:", this.modConfig.configOptions.fleaMinimumLevel)
-            console.log("[DEBUG] [SCHKRM] Basic Flea Changer - Flea market only accepts FIR items set to:", this.modConfig.configOptions.fleaSellingFIROnly)
-        }
-        
         logger.logWithColor("[SCHKRM] Basic Flea Changer loaded.", LogTextColor.BLACK, LogBackgroundColor.YELLOW);
     }
 }
